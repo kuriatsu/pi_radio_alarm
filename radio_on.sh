d2114a72fc1d70495df013e54f2dbfddb7d0c3c6
Kuribayashi Atsushi <38074802+kuriatsu@users.noreply.github.com>
2021-05-02 19:08:51 +0900

    Update radio_on.sh

--- radio_on.sh
+++ radio_on.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rtl_fm -f 77600000 -s 1000000 -r 60000 - | aplay -r 60000 -f S16_LE -t raw plughw:Device
+rtl_fm -f 77600000 -s 1000000 -r 60000 - | aplay -r 60000 -f S16_LE -t raw -D plughw:Device
